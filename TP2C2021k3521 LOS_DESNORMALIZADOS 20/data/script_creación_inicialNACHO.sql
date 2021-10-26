5b4530bda6a71daf8a9d0150e03f3c56e371d697
caroblanco <carolina.blancos@gmail.com>
2021-10-26 01:12:00 -0300

    murir

--- TP2C2021k3521 LOS_DESNORMALIZADOS 20/data/script_creación_inicialNACHO.sql
+++ TP2C2021k3521 LOS_DESNORMALIZADOS 20/data/script_creación_inicialNACHO.sql
@@ -9,6 +9,153 @@ IF(NOT EXISTS(SELECT * FROM INFORMATION_SCHEMA.SCHEMATA WHERE SCHEMA_NAME = 'los
 
 --CREAMOS LAS TABLAS
 
+IF OBJECT_ID ('los_desnormalizados.Viaje_x_paquete', 'U') IS NOT NULL  
+   DROP TABLE los_desnormalizados.Viaje_x_paquete; 
+GO
+CREATE TABLE los_desnormalizados.Viaje_x_paquete (
+	paquete_id INT NOT NULL,
+	viaje_id INT NOT NULL,
+	cantidad INT NOT NULL,
+	precioTotal FLOAT NOT NULL,
+	PRIMARY KEY (paquete_id, viaje_id)
+)
+
+
+IF OBJECT_ID ('los_desnormalizados.Viaje', 'U') IS NOT NULL  
+   DROP TABLE los_desnormalizados.Viaje; 
+GO
+CREATE TABLE los_desnormalizados.Viaje (
+	viaje_id INT IDENTITY(1,1) PRIMARY KEY,
+	camion_id INT NOT NULL,
+	recorrido_id INT NOT NULL,
+	chofer INT NOT NULL,
+	fecha_inicio DATETIME2(7) NOT NULL,
+	fecha_fin DATETIME2(3) NULL,
+	lts_combustible DECIMAL(18,2) NULL
+)
+
+
+IF OBJECT_ID ('los_desnormalizados.Recorrido', 'U') IS NOT NULL  
+   DROP TABLE los_desnormalizados.Recorrido; 
+GO
+CREATE TABLE los_desnormalizados.Recorrido (
+	recorrido_id INT IDENTITY(1,1) PRIMARY KEY,
+	ciudad_origen_id INT	NOT NULL,
+	ciudad_destino_id INT	NOT NULL,
+	km_recorridos INT		NOT NULL,
+	precio DECIMAL(18,2)	NOT NULL
+)
+
+IF OBJECT_ID ('los_desnormalizados.Tarea_x_orden', 'U') IS NOT NULL  
+   DROP TABLE los_desnormalizados.Tarea_x_orden; 
+GO
+CREATE TABLE los_desnormalizados.Tarea_x_orden(
+	tarea_x_orden_id INT IDENTITY(1,1) PRIMARY KEY,
+    	orden_id INT NOT NULL,
+    	tarea_id INT NOT NULL,
+    	mecanico_id INT NOT NULL,
+    	inicio_planificado DATETIME2 NOT NULL,
+   	inicio_real DATETIME2 NULL,
+    	fin_real DATETIME2 NULL,
+	tiempo_real INT NULL
+)
+
+
+IF OBJECT_ID ('los_desnormalizados.Mecanico', 'U') IS NOT NULL  
+   DROP TABLE los_desnormalizados.Mecanico; 
+GO
+CREATE TABLE los_desnormalizados.Mecanico(
+	legajo INT PRIMARY KEY,
+	nombre NVARCHAR(255) NOT NULL,
+	apellido NVARCHAR(255) NOT NULL,
+	dni DECIMAL(18,0) NOT NULL,
+	direccion NVARCHAR(255) NOT NULL,
+	telefono INT NOT NULL,
+	mail NVARCHAR(255) NOT NULL,
+	fecha_nacimiento DATETIME2(3) NOT NULL,
+	costo_hora INT NOT NULL,
+	taller_id INT NOT NULL
+)
+
+
+IF OBJECT_ID ('los_desnormalizados.Taller', 'U') IS NOT NULL  
+   DROP TABLE los_desnormalizados.Taller; 
+GO
+CREATE TABLE los_desnormalizados.Taller(
+	taller_id INT IDENTITY(1,1) PRIMARY KEY, 
+	ciudad_id INT NOT NULL,
+	nombre NVARCHAR(255) NOT NULL,
+	telefono DECIMAL(18,0) NOT NULL,
+	direccion NVARCHAR(255) NOT NULL,
+	mail NVARCHAR(255) NOT NULL
+)
+
+IF OBJECT_ID ('los_desnormalizados.Orden_trabajo', 'U') IS NOT NULL  
+   DROP TABLE los_desnormalizados.Orden_trabajo; 
+GO
+CREATE TABLE los_desnormalizados.Orden_trabajo (
+	orden_id INT IDENTITY(1,1) PRIMARY KEY,
+	fecha_generacion NVARCHAR(255) NOT NULL,
+	camion_id INT NOT NULL,
+	estado_id INT NOT NULL
+)
+
+
+IF OBJECT_ID ('los_desnormalizados.Camion', 'U') IS NOT NULL  
+   DROP TABLE los_desnormalizados.Camion; 
+GO
+CREATE TABLE los_desnormalizados.Camion(
+	camion_id INT IDENTITY(1,1) PRIMARY KEY, 
+	modelo_id INT NOT NULL,
+	patente NVARCHAR(255) NOT NULL,
+	chasis NVARCHAR(255) NOT NULL,
+	motor NVARCHAR(255) NOT NULL,
+	fecha_alta DATETIME2(3) NOT NULL
+)
+
+IF OBJECT_ID ('los_desnormalizados.Modelo', 'U') IS NOT NULL  
+   DROP TABLE los_desnormalizados.Modelo; 
+GO
+CREATE TABLE los_desnormalizados.Modelo(
+	modelo_id INT IDENTITY(1,1) PRIMARY KEY,
+	marca_id INT NOT NULL,
+	modelo_descripcion NVARCHAR(255) NOT NULL,
+	velocidad_max INT NOT NULL,
+	capacidad_tanque INT NOT NULL,
+	capacidad_carga INT NOT NULL
+)
+
+IF OBJECT_ID ('los_desnormalizados.Material_x_tarea', 'U') IS NOT NULL  
+   DROP TABLE los_desnormalizados.Material_x_tarea; 
+GO
+CREATE TABLE  los_desnormalizados.Material_x_tarea(
+    	material_id INT NOT NULL,
+    	tarea_id INT NOT NULL, 
+	cant_material INT NOT NULL,
+	PRIMARY KEY (material_id, tarea_id)
+)
+
+
+IF OBJECT_ID ('los_desnormalizados.Tarea', 'U') IS NOT NULL  
+   DROP TABLE los_desnormalizados.Tarea; 
+GO
+CREATE TABLE  los_desnormalizados.Tarea (
+    	tarea_id INT PRIMARY KEY,
+    	tipo_tarea_id INT NOT NULL,
+    	tiempo_estimado INT NOT NULL, 
+    	descripcion NVARCHAR(255) NOT NULL
+)
+
+
+IF OBJECT_ID ('los_desnormalizados.Paquete', 'U') IS NOT NULL  
+   DROP TABLE los_desnormalizados.Paquete; 
+GO
+CREATE TABLE los_desnormalizados.Paquete (
+	paquete_id INT IDENTITY(1,1) PRIMARY KEY,
+	tipo_paquete_id INT NOT NULL
+)
+
+
 IF OBJECT_ID ('los_desnormalizados.Chofer', 'U') IS NOT NULL  
    DROP TABLE los_desnormalizados.Chofer; 
 GO
@@ -21,7 +168,7 @@ CREATE TABLE los_desnormalizados.Chofer (
 	telefono INT			NOT NULL,
 	mail NVARCHAR(255)		NOT NULL,
 	fecha_nac DATETIME2(3)	NOT NULL,
-	costo_hora INT			NOT NULL,
+	costo_hora INT			NOT NULL
 )
 
 IF OBJECT_ID ('los_desnormalizados.Ciudad', 'U') IS NOT NULL  
@@ -29,21 +176,10 @@ IF OBJECT_ID ('los_desnormalizados.Ciudad', 'U') IS NOT NULL
 GO
 CREATE TABLE los_desnormalizados.Ciudad (
 	ciudad_id INT IDENTITY(1,1) PRIMARY KEY,
-	nombre NVARCHAR(255) NOT NULL,
+	nombre NVARCHAR(255) NOT NULL
 )
 
-IF OBJECT_ID ('los_desnormalizados.Recorrido', 'U') IS NOT NULL  
-   DROP TABLE los_desnormalizados.Recorrido; 
-GO
-CREATE TABLE los_desnormalizados.Recorrido (
-	recorrido_id INT IDENTITY(1,1) PRIMARY KEY,
-	ciudad_origen_id INT	NOT NULL,
-	ciudad_destino_id INT	NOT NULL,
-	km_recorridos INT		NOT NULL,
-	precio DECIMAL(18,2)	NOT NULL,
-	FOREIGN KEY (ciudad_destino_id) REFERENCES los_desnormalizados.Ciudad (ciudad_id),
-	FOREIGN KEY (ciudad_destino_id) REFERENCES los_desnormalizados.Ciudad (ciudad_id)
-)
+
 
 IF OBJECT_ID ('los_desnormalizados.Tipo_paquete', 'U') IS NOT NULL  
    DROP TABLE los_desnormalizados.Tipo_paquete; 
@@ -63,34 +199,11 @@ IF OBJECT_ID ('los_desnormalizados.Marca', 'U') IS NOT NULL
 GO
 CREATE TABLE los_desnormalizados.Marca (
 	marca_id INT IDENTITY(1,1) PRIMARY KEY,
-	nombre NVARCHAR(255) NOT NULL,
+	nombre NVARCHAR(255) NOT NULL
 )
 
-IF OBJECT_ID ('los_desnormalizados.Modelo', 'U') IS NOT NULL  
-   DROP TABLE los_desnormalizados.Modelo; 
-GO
-CREATE TABLE los_desnormalizados.Modelo(
-	modelo_id INT IDENTITY(1,1) PRIMARY KEY,
-	marca_id INT NOT NULL,
-	modelo_descripcion NVARCHAR(255) NOT NULL,
-	velocidad_max INT NOT NULL,
-	capacidad_tanque INT NOT NULL,
-	capacidad_carga INT NOT NULL,
-	FOREIGN KEY (marca_id) REFERENCES los_desnormalizados.Marca (marca_id)
-)
 
-IF OBJECT_ID ('los_desnormalizados.Taller', 'U') IS NOT NULL  
-   DROP TABLE los_desnormalizados.Taller; 
-GO
-CREATE TABLE los_desnormalizados.Taller(
-	taller_id INT IDENTITY(1,1) PRIMARY KEY, 
-	ciudad_id INT NOT NULL,
-	nombre NVARCHAR(255) NOT NULL,
-	telefono DECIMAL(18,0) NOT NULL,
-	direccion NVARCHAR(255) NOT NULL,
-	mail NVARCHAR(255) NOT NULL,
-	FOREIGN KEY (ciudad_id) REFERENCES los_desnormalizados.Ciudad (ciudad_id)
-)
+
 
 IF OBJECT_ID ('los_desnormalizados.Material', 'U') IS NOT NULL  
    DROP TABLE los_desnormalizados.Material; 
@@ -99,7 +212,7 @@ CREATE TABLE  los_desnormalizados.Material (
     	material_id INT IDENTITY(1,1) PRIMARY KEY,
 	material_cod NVARCHAR(100) NOT NULL,
     	material_descripcion NVARCHAR(255) NOT NULL,
-    	precio DECIMAL(18, 2) NOT NULL,
+    	precio DECIMAL(18, 2) NOT NULL
 )
 
 IF OBJECT_ID ('los_desnormalizados.Tipo_tarea', 'U') IS NOT NULL  
@@ -107,136 +220,18 @@ IF OBJECT_ID ('los_desnormalizados.Tipo_tarea', 'U') IS NOT NULL
 GO
 CREATE TABLE los_desnormalizados.Tipo_tarea (
 	tipo_tarea_id INT IDENTITY(1,1) PRIMARY KEY,
-	descripcion NVARCHAR(255) NOT NULL,
+	descripcion NVARCHAR(255) NOT NULL
 )
 
-IF OBJECT_ID ('los_desnormalizados.Tarea', 'U') IS NOT NULL  
-   DROP TABLE los_desnormalizados.Tarea; 
-GO
-CREATE TABLE  los_desnormalizados.Tarea (
-    	tarea_id INT PRIMARY KEY,
-    	tipo_tarea_id INT NOT NULL,
-    	tiempo_estimado INT NOT NULL, 
-    	descripcion NVARCHAR(255) NOT NULL,
-	FOREIGN KEY (tipo_tarea_id) REFERENCES los_desnormalizados.Tipo_tarea (tipo_tarea_id)
-)
-
-IF OBJECT_ID ('los_desnormalizados.Camion', 'U') IS NOT NULL  
-   DROP TABLE los_desnormalizados.Camion; 
-GO
-CREATE TABLE los_desnormalizados.Camion(
-	camion_id INT IDENTITY(1,1) PRIMARY KEY, 
-	modelo_id INT NOT NULL,
-	patente NVARCHAR(255) NOT NULL,
-	chasis NVARCHAR(255) NOT NULL,
-	motor NVARCHAR(255) NOT NULL,
-	fecha_alta DATETIME2(3) NOT NULL,
-	FOREIGN KEY (modelo_id) REFERENCES los_desnormalizados.Modelo (modelo_id)
-)
-
-IF OBJECT_ID ('los_desnormalizados.Viaje', 'U') IS NOT NULL  
-   DROP TABLE los_desnormalizados.Viaje; 
-GO
-CREATE TABLE los_desnormalizados.Viaje (
-	viaje_id INT IDENTITY(1,1) PRIMARY KEY,
-	camion_id INT NOT NULL,
-	recorrido_id INT NOT NULL,
-	chofer INT NOT NULL,
-	fecha_inicio DATETIME2(7) NOT NULL,
-	fecha_fin DATETIME2(3) NULL,
-	lts_combustible DECIMAL(18,2) NULL, 
-	FOREIGN KEY (camion_id)	REFERENCES los_desnormalizados.Camion (camion_id),
-	FOREIGN KEY (chofer)	REFERENCES los_desnormalizados.Chofer (legajo),
-	FOREIGN KEY (recorrido_id) REFERENCES los_desnormalizados.Recorrido (recorrido_id)
-)
-
-IF OBJECT_ID ('los_desnormalizados.Paquete', 'U') IS NOT NULL  
-   DROP TABLE los_desnormalizados.Paquete; 
-GO
-CREATE TABLE los_desnormalizados.Paquete (
-	paquete_id INT IDENTITY(1,1) PRIMARY KEY,
-	tipo_paquete_id INT NOT NULL,
-	FOREIGN KEY (tipo_paquete_id) REFERENCES los_desnormalizados.Tipo_paquete(tipo_paquete_id)
-)
-
-IF OBJECT_ID ('los_desnormalizados.Viaje_x_paquete', 'U') IS NOT NULL  
-   DROP TABLE los_desnormalizados.Viaje_x_paquete; 
-GO
-CREATE TABLE los_desnormalizados.Viaje_x_paquete (
-	paquete_id INT NOT NULL,
-	viaje_id INT NOT NULL,
-	cantidad INT NOT NULL,
-	precioTotal FLOAT NOT NULL,
-	PRIMARY KEY (paquete_id, viaje_id),
-	FOREIGN KEY (paquete_id) REFERENCES los_desnormalizados.Paquete(paquete_id),
-	FOREIGN KEY (viaje_id) REFERENCES los_desnormalizados.Viaje(viaje_id)
-)
 
 IF OBJECT_ID ('los_desnormalizados.Estado', 'U') IS NOT NULL  
    DROP TABLE los_desnormalizados.Estado; 
 GO
 CREATE TABLE los_desnormalizados.Estado (
 	estado_id INT IDENTITY(1,1) PRIMARY KEY,
-	descripcion NVARCHAR(255) NOT NULL,
+	descripcion NVARCHAR(255) NOT NULL
 )
 
-IF OBJECT_ID ('los_desnormalizados.Orden_trabajo', 'U') IS NOT NULL  
-   DROP TABLE los_desnormalizados.Orden_trabajo; 
-GO
-CREATE TABLE los_desnormalizados.Orden_trabajo (
-	orden_id INT IDENTITY(1,1) PRIMARY KEY,
-	fecha_generacion NVARCHAR(255) NOT NULL,
-	camion_id INT NOT NULL,
-	estado_id INT NOT NULL,
-	FOREIGN KEY (camion_id) REFERENCES los_desnormalizados.Camion(camion_id),
-	FOREIGN KEY (estado_id) REFERENCES los_desnormalizados.Estado(estado_id)
-)
-
-IF OBJECT_ID ('los_desnormalizados.Mecanico', 'U') IS NOT NULL  
-   DROP TABLE los_desnormalizados.Mecanico; 
-GO
-CREATE TABLE los_desnormalizados.Mecanico(
-	legajo INT PRIMARY KEY,
-	nombre NVARCHAR(255) NOT NULL,
-	apellido NVARCHAR(255) NOT NULL,
-	dni DECIMAL(18,0) NOT NULL,
-	direccion NVARCHAR(255) NOT NULL,
-	telefono INT NOT NULL,
-	mail NVARCHAR(255) NOT NULL,
-	fecha_nacimiento DATETIME2(3) NOT NULL,
-	costo_hora INT NOT NULL,
-	taller_id INT NOT NULL,
-	FOREIGN KEY (taller_id) REFERENCES los_desnormalizados.Taller (taller_id)
-)
-
-IF OBJECT_ID ('los_desnormalizados.Tarea_x_orden', 'U') IS NOT NULL  
-   DROP TABLE los_desnormalizados.Tarea_x_orden; 
-GO
-CREATE TABLE los_desnormalizados.Tarea_x_orden(
-	tarea_x_orden_id INT IDENTITY(1,1) PRIMARY KEY,
-    	orden_id INT NOT NULL,
-    	tarea_id INT NOT NULL,
-    	mecanico_id INT NOT NULL,
-    	inicio_planificado DATETIME2 NOT NULL,
-   	inicio_real DATETIME2 NULL,
-    	fin_real DATETIME2 NULL,
-	tiempo_real INT NULL,
-    	FOREIGN KEY (tarea_id) references los_desnormalizados.Tarea (tarea_id),
-    	FOREIGN KEY (mecanico_id) references los_desnormalizados.Mecanico (legajo),
-    	FOREIGN KEY (orden_id) references  los_desnormalizados.Orden_trabajo (orden_id)
-)
-
-IF OBJECT_ID ('los_desnormalizados.Material_x_tarea', 'U') IS NOT NULL  
-   DROP TABLE los_desnormalizados.Material_x_tarea; 
-GO
-CREATE TABLE  los_desnormalizados.Material_x_tarea(
-    	material_id INT NOT NULL,
-    	tarea_id INT NOT NULL, 
-	cant_material INT NOT NULL,
-	PRIMARY KEY (material_id, tarea_id),
-    	FOREIGN KEY (material_id) REFERENCES  los_desnormalizados.Material (material_id),
-    	FOREIGN KEY (tarea_id) REFERENCES  los_desnormalizados.Tarea (tarea_id)
-)
 
 
 --CREAMOS PROCEDURES PARA LA MIGRACION HACIA LAS TABLAS CREADAS PREVIAMENTE
@@ -555,6 +550,7 @@ GO
 CREATE PROCEDURE los_desnormalizados.migracion
 as
 begin
+
 	exec los_desnormalizados.migracionChofer
 	exec los_desnormalizados.migracionCiudad
 	exec los_desnormalizados.migracionRecorrido
@@ -580,3 +576,72 @@ GO
 exec los_desnormalizados.migracion
 
 
+-- CONSTRAINTS
+
+ALTER TABLE los_desnormalizados.Viaje 
+ADD	FOREIGN KEY (camion_id)	REFERENCES los_desnormalizados.Camion (camion_id),
+	FOREIGN KEY (chofer)	REFERENCES los_desnormalizados.Chofer (legajo),
+	FOREIGN KEY (recorrido_id) REFERENCES los_desnormalizados.Recorrido (recorrido_id)
+GO
+
+
+ALTER TABLE los_desnormalizados.Viaje_x_paquete 
+ADD
+	FOREIGN KEY (paquete_id) REFERENCES los_desnormalizados.Paquete(paquete_id),
+	FOREIGN KEY (viaje_id) REFERENCES los_desnormalizados.Viaje(viaje_id)
+GO
+
+
+
+ALTER TABLE los_desnormalizados.Recorrido
+ADD
+	FOREIGN KEY (ciudad_destino_id) REFERENCES los_desnormalizados.Ciudad (ciudad_id),
+	FOREIGN KEY (ciudad_destino_id) REFERENCES los_desnormalizados.Ciudad (ciudad_id)
+GO
+
+ALTER TABLE los_desnormalizados.Taller
+ADD	FOREIGN KEY (ciudad_id) REFERENCES los_desnormalizados.Ciudad (ciudad_id)
+GO
+
+ALTER TABLE los_desnormalizados.Modelo
+ADD	FOREIGN KEY (marca_id) REFERENCES los_desnormalizados.Marca (marca_id)
+
+
+ALTER TABLE  los_desnormalizados.Tarea 
+ADD	FOREIGN KEY (tipo_tarea_id) REFERENCES los_desnormalizados.Tipo_tarea (tipo_tarea_id)
+GO
+
+ALTER TABLE los_desnormalizados.Camion
+ADD	FOREIGN KEY (modelo_id) REFERENCES los_desnormalizados.Modelo (modelo_id)
+GO
+
+
+ALTER TABLE los_desnormalizados.Paquete 
+ADD
+	FOREIGN KEY (tipo_paquete_id) REFERENCES los_desnormalizados.Tipo_paquete(tipo_paquete_id)
+GO
+
+
+ALTER TABLE los_desnormalizados.Orden_trabajo 
+ADD
+	FOREIGN KEY (camion_id) REFERENCES los_desnormalizados.Camion(camion_id),
+	FOREIGN KEY (estado_id) REFERENCES los_desnormalizados.Estado(estado_id)
+GO
+
+ALTER TABLE los_desnormalizados.Mecanico 
+ADD
+	FOREIGN KEY (taller_id) REFERENCES los_desnormalizados.Taller (taller_id)
+GO
+
+ALTER TABLE los_desnormalizados.Tarea_x_orden
+ADD
+    	FOREIGN KEY (tarea_id) references los_desnormalizados.Tarea (tarea_id),
+    	FOREIGN KEY (mecanico_id) references los_desnormalizados.Mecanico (legajo),
+    	FOREIGN KEY (orden_id) references  los_desnormalizados.Orden_trabajo (orden_id)
+GO
+
+ALTER TABLE  los_desnormalizados.Material_x_tarea
+ADD
+    	FOREIGN KEY (material_id) REFERENCES  los_desnormalizados.Material (material_id),
+    	FOREIGN KEY (tarea_id) REFERENCES  los_desnormalizados.Tarea (tarea_id)
+GO
