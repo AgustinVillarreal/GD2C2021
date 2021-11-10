5160e16e151e47458f10e795015debee02f59a09
AgustinVillarreal <62357481+AgustinVillarreal@users.noreply.github.com>
2021-11-09 21:43:45 -0300

    Creacion de tablas de dimension y migracion

---
+++ script_creacion_BI.sql
@@ -0,0 +1,158 @@
+USE GD2C2021
+
+--DROP PREVENTIVO DE FUNCIONES------------------------------------------------------------
+
+IF EXISTS(SELECT [name] FROM sys.objects WHERE [name] = 'getAgeRange')
+	DROP FUNCTION los_desnormalizados.getAgeRange
+
+--DROP PREVENTIVO DE TABLAS------------------------------------------------------------
+IF EXISTS(SELECT [name] FROM sys.tables WHERE [name] = 'BI_DIM_TALLER')
+DROP TABLE  los_desnormalizados.BI_DIM_TALLER
+
+IF EXISTS(SELECT [name] FROM sys.tables WHERE [name] = 'BI_DIM_RECORRIDO')
+DROP TABLE  los_desnormalizados.BI_DIM_RECORRIDO
+
+IF EXISTS(SELECT [name] FROM sys.tables WHERE [name] = 'BI_DIM_TIPO_TAREA')
+DROP TABLE  los_desnormalizados.BI_DIM_TIPO_TAREA
+
+IF EXISTS(SELECT [name] FROM sys.tables WHERE [name] = 'BI_DIM_CHOFER')
+DROP TABLE  los_desnormalizados.BI_DIM_CHOFER
+
+IF EXISTS(SELECT [name] FROM sys.tables WHERE [name] = 'BI_DIM_MECANICO')
+DROP TABLE  los_desnormalizados.BI_DIM_MECANICO
+
+--CREACIÓN DE FUNCIONES AUXILIARES------------------------------------------------------------
+GO
+CREATE FUNCTION los_desnormalizados.getAgeRange (@dateofbirth datetime2(3)) --Recibe una fecha de nacimiento por parámetro y 
+RETURNS varchar(10)												   --devuelve la edad actual de la persona		
+AS
+BEGIN
+	DECLARE @age int;
+	DECLARE @returnvalue varchar(10);
+
+IF (MONTH(@dateofbirth)!=MONTH(GETDATE()))
+	SET @age = DATEDIFF(MONTH, @dateofbirth, GETDATE())/12;
+ELSE IF(DAY(@dateofbirth) > DAY(GETDATE()))
+	SET @age = (DATEDIFF(MONTH, @dateofbirth, GETDATE())/12)-1;
+ELSE 
+	SET @age = DATEDIFF(MONTH, @dateofbirth, GETDATE())/12;
+
+IF (@age > 17 AND @age <31)
+BEGIN
+	SET @returnvalue = '[18 - 30]';
+END
+ELSE IF (@age > 30 AND @age <51)
+BEGIN
+	SET @returnvalue = '[31 - 50]';
+END
+ELSE IF(@age > 50)
+BEGIN
+	SET @returnvalue = '+50';
+END
+
+	RETURN @returnvalue;
+END
+
+GO
+
+--Creación y migración de las tablas de las dimensiones
+
+--DIMENSION TALLER
+CREATE TABLE los_desnormalizados.BI_DIM_TALLER(
+	taller_id INT PRIMARY KEY, 
+	nombre NVARCHAR(255) NOT NULL,
+	telefono DECIMAL(18,0) NOT NULL,
+	direccion NVARCHAR(255) NOT NULL,
+	mail NVARCHAR(255) NOT NULL,
+)
+
+
+INSERT INTO los_desnormalizados.BI_DIM_TALLER (taller_id, nombre, telefono, direccion, mail) 
+	SELECT taller_id, nombre, telefono, direccion, mail FROM los_desnormalizados.Taller
+
+--DIMENSION TIPO_TAREA
+CREATE TABLE los_desnormalizados.BI_DIM_TIPO_TAREA(
+	tipo_tarea_id INT PRIMARY KEY,
+	descripcion NVARCHAR(255) NOT NULL,
+)
+
+
+INSERT INTO los_desnormalizados.BI_DIM_TIPO_TAREA (tipo_tarea_id, descripcion) 
+	SELECT tipo_tarea_id, descripcion FROM los_desnormalizados.Tipo_tarea
+
+--DIMENSION RECORRIDO
+CREATE TABLE los_desnormalizados.BI_DIM_RECORRIDO (
+	recorrido_id INT PRIMARY KEY,
+	km_recorridos INT		NOT NULL,
+	precio DECIMAL(18,2)	NOT NULL,
+)
+
+INSERT INTO los_desnormalizados.BI_DIM_RECORRIDO (recorrido_id, km_recorridos, precio) 
+	SELECT recorrido_id, km_recorridos, precio FROM los_desnormalizados.Recorrido
+
+--DIMENSION CHOFER
+CREATE TABLE los_desnormalizados.BI_DIM_CHOFER(
+	legajo	INT PRIMARY KEY,
+	nombre NVARCHAR(255)	NOT NULL,
+	apellido NVARCHAR(255)	NOT NULL,
+	dni DECIMAL(18,0)		NOT NULL,
+	direccion NVARCHAR(255) NOT NULL,
+	telefono INT			NOT NULL,
+	mail NVARCHAR(255)		NOT NULL,
+	fecha_nac DATETIME2(3)	NOT NULL,
+	costo_hora INT			NOT NULL,
+	rango_edad NVARCHAR(10) NOT NULL,
+)
+
+INSERT INTO los_desnormalizados.BI_DIM_CHOFER
+	(legajo, nombre, apellido, dni, direccion, telefono, mail, fecha_nac, costo_hora, rango_edad) 
+	SELECT  
+	legajo, nombre, apellido, dni, direccion, telefono, mail, fecha_nac, costo_hora, los_desnormalizados.getAgeRange(fecha_nac) 
+	FROM los_desnormalizados.Chofer
+
+--DIMENSION MECANICO
+CREATE TABLE los_desnormalizados.BI_DIM_MECANICO(
+	legajo INT PRIMARY KEY,
+	nombre NVARCHAR(255) NOT NULL,
+	apellido NVARCHAR(255) NOT NULL,
+	dni DECIMAL(18,0) NOT NULL,
+	direccion NVARCHAR(255) NOT NULL,
+	telefono INT NOT NULL,
+	mail NVARCHAR(255) NOT NULL,
+	fecha_nacimiento DATETIME2(3) NOT NULL,
+	costo_hora INT NOT NULL,
+	rango_edad NVARCHAR(10) NOT NULL,
+)
+
+INSERT INTO los_desnormalizados.BI_DIM_MECANICO
+	(legajo, nombre, apellido, dni, direccion, telefono, mail, fecha_nacimiento, costo_hora, rango_edad) 
+	SELECT  
+	legajo, nombre, apellido, dni, direccion, telefono, mail, fecha_nacimiento, costo_hora, los_desnormalizados.getAgeRange(fecha_nacimiento) 
+	FROM los_desnormalizados.Mecanico
+
+--DIMENSION MATERIAL (agregada)
+CREATE TABLE los_desnormalizados.BI_DIM_MATERIAL (
+    material_id INT PRIMARY KEY,
+	material_cod NVARCHAR(100) NOT NULL,
+    material_descripcion NVARCHAR(255) NOT NULL,
+    precio DECIMAL(18, 2) NOT NULL,
+)
+
+INSERT INTO los_desnormalizados.BI_DIM_MATERIAL (material_id, material_cod, material_descripcion, precio)
+	SELECT material_id, material_cod, material_descripcion, precio FROM los_desnormalizados.Material
+
+
+--Creación y migración de las tablas de hechos
+
+
+-- TABLA DE HECHO: MATERIAL_TALLER
+CREATE TABLE los_desnormalizados.BI_FACT_MATERIAL_TALLER(
+    material_id INT REFERENCES los_desnormalizados.BI_DIM_MATERIAL,
+	taller_id INT REFERENCES los_desnormalizados.BI_DIM_TALLER,
+	cantidad INT NOT NULL,
+	PRIMARY KEY(material_id, taller_id)
+)
+/*
+INSERT INTO los_desnormalizados.BI_FACT_MATERIAL_TALLER (material_id, taller_id, cantidad) 
+	SELECT material_id, taller_id,
+*/
